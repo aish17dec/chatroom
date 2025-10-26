e94cd6f2f809e16139169b074562761052b8045f
Vivek Bhadra <vivek.bhadra@gmail.com>
2025-10-26 10:21:52 +0000

    Correcting client 1 start script.

--- start_client1.sh
+++ start_client1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./bin/client   --user Lucy   --self-id 1   --peer-id 2   --listen 0.0.0.0:8001   --peer 172:   --server 172.31.23.9:7000
+./bin/client   --user Lucy   --self-id 1   --peer-id 2   --listen 0.0.0.0:8001   --peer 172.31.27.84:8002 --server 172.31.23.9:7000
