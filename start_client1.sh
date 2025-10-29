211038099bde1e57bbf0d8afe262a6dd8ebacb16
aish17dec <aish.17dec@gmail.com>
2025-10-29 19:05:51 +0530

    Update start_client1.sh
    updated ip address of peer and server

--- start_client1.sh
+++ start_client1.sh
@@ -2,7 +2,7 @@
 # start_client1.sh
 # Purpose: Print and execute the client command for Lucy (Client 1)
 
-CMD="./bin/client --user Lucy --self-id 1 --peer-id 2 --listen 0.0.0.0:8001 --peer 172.31.27.84:8002 --server 172.31.23.9:7000"
+CMD="./bin/client --user Lucy --self-id 1 --peer-id 2 --listen 0.0.0.0:8001 --peer 10.0.0.9:8002 --server 10.0.0.13:7000"
 
 # Print the command
 echo "Executing: $CMD"
