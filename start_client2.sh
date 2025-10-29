8cd1638b73bb12912c48b5d4795c3bdc9c6705d8
aish17dec <aish.17dec@gmail.com>
2025-10-29 19:08:19 +0530

    Update start_client2.sh
    updated ip for server and peer

--- start_client2.sh
+++ start_client2.sh
@@ -2,7 +2,7 @@
 # start_client2.sh
 # Purpose: Print and execute the client command for Joel (Client 2)
 
-CMD="./bin/client --user \"Joel\" --self-id 2 --peer-id 1 --listen 0.0.0.0:8002 --peer 172.31.22.222:8001 --server 172.31.23.9:7000"
+CMD="./bin/client --user \"Joel\" --self-id 2 --peer-id 1 --listen 0.0.0.0:8002 --peer 10.0.0.5:8001 --server 10.0.0.13:7000"
 
 # Print the command
 echo "Executing: $CMD"
